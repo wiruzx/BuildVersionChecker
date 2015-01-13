5db4a994f0b2de2b5c0bdf23d79e78208bdbfe93
Victor Shamanov <wiruzx@gmail.com>
2015-01-13 14:34:52 +0300

    Fix executing PlistBuddy

--- main.py
+++ main.py
@@ -2,20 +2,26 @@
 
 import os
 import subprocess
+import sys
 
-def execute(command):
-    return subprocess.Popen(command, stdout=PIPE).stdout.read()
+def execute(args):
+    return subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
 
 def get_from_plist(key, plist):
-    return execute('/usr/libexec/PlistBuddy -c "Print %s" "%s"' % (key, plist))
+    return execute(['/usr/libexec/PlistBuddy', '-c', 'Print %s' % key, '%s' % plist])
 
-plist = os.environ['INFOPLIST_FILE']
-build_version = get_from_plist('CFBundleVersion', plist)
-short_build_version = get_from_plist('CFBundleShortVersion', plist)
+def escape_newline(string):
+    return string.replace('\n', '')
+
+src_root = os.environ['SRCROOT']
+plist = src_root + '/' + os.environ['INFOPLIST_FILE']
+build_version = escape_newline(get_from_plist('CFBundleVersion', plist))
+short_build_version = escape_newline(get_from_plist('CFBundleShortVersionString', plist))
 
 if build_version.startswith(short_build_version):
     sys.exit(0)
 else:
+    print("\nBuild (%s) should starts with Version (%s)\n" % (build_version, short_build_version))
     sys.exit(1)
 
 
